5e91f9d99dcb8f41635d9b6823aee4ee1511a6a2
SereGGA N <sergei.ni@yandex.ru>
2020-05-17 00:44:18 +0300

    frontend

---
+++ frontend/myapp/src/shared/UI/ErrorModal.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Modal from "./Modal";
+import Button from "./Button";
+
+const ErrorModal = (props) => {
+  return (
+    <Modal
+      onCancel={props.onClear}
+      header="An Error Occurred!"
+      show={!!props.error}
+      footer={
+        <Button
+          onClick={props.onClear}
+          className="center primary danger center"
+        >
+          Okay
+        </Button>
+      }
+    >
+      <p>{props.error}</p>
+    </Modal>
+  );
+};
+
+export default ErrorModal;
